ecf3491ad313fc0a812652adad01eedf56edeef1
Victorpri <vimapri1234@gmail.com>
2023-02-27 13:51:47 -0500

    subir

--- excepciones/excepB.py
+++ excepciones/excepB.py
@@ -8,6 +8,13 @@ try:
 # en esta parte se utilizan dos variables separadas por comas las cuales valen el divmod(se utiliza para conseguir el cosiente y el residuo)
     
     print(f'q={q}')
+#no me acuerdo
+
     print(f'r={r}')
+#no me acuerdo
+
 except (ZeroDivisionError, TypeError) as e:
+#este se usa parte se crea un except con dos errors, es decir que si hay alguno de los dos errores dara el mismo mensaje
+
     print(type(e), e)
+#este es el resultado de el except pero no me acuerdo como funciona
