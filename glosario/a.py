96e45c34340309750fefa9c43eb479f05eb4ad0c
Victorpri <vimapri1234@gmail.com>
2023-02-27 13:43:08 -0500

    subir archivo

---
+++ glosario/a.py
@@ -0,0 +1,37 @@
+'''roles
+
+metodologia agil
+
+experto de dominio
+
+stakeholdes partes interesadas
+
+analista 
+
+procesos agiles
+
+administrador de sistemas
+
+servidores
+
+DevOps operaciones y desarrollos
+
+SRE(el ingeniero de la confianza)
+
+QA- tester
+
+desarrollador
+
+arquitecto
+
+equipo de desarrollo
+
+metodologias tradicionales
+
+gestor de proyecto
+
+facilitadores
+
+
+
+'''
