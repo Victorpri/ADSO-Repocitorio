c3b34b9cdae3adeaf7d0160e03cc519e85807fbe
Victorpri <vimapri1234@gmail.com>
2023-03-03 16:29:20 -0500

    subir

---
+++ glosario/glosario6.py
@@ -0,0 +1,42 @@
+'''
+objetivos ¿que quieren?:::::::
+
+cliente
+
+manager
+
+Dev
+
+Usuario
+
+QA
+
+____________________________________
+
+Arquitectura y metodologias
+
+agil:
+
+
+
+
+
+tadicionales:
+
+etapa de diseño
+modelo de arquitectura
+
+prioridades del blacklog
+planteaminto del sprint
+metricas
+
+flexibilidad delsistema
+
+
+
+
+20
+
+
+
+'''
